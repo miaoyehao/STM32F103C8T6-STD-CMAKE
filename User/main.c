3cd736b85d54dc6fd5be821758e448ed07cc84bb
miaoyehao <2394785879@qq.com>
2025-03-25 21:23:33 +0800

    Updata

--- User/main.c
+++ User/main.c
@@ -1,30 +1,35 @@
 #include "stm32f10x.h"                  // Device header
 #include "Delay.h"
 #include "OLED.h"
-#include "MPU6050.h"
+#include "MyFLASH.h"
+#include "Key.h"
 
-uint8_t ID;								//定义用于存放ID号的变量
-int16_t AX, AY, AZ, GX, GY, GZ;			//定义用于存放各个数据的变量
+uint8_t KeyNum;
 
 int main(void)
 {
 	/*模块初始化*/
 	OLED_Init();		//OLED初始化
-	MPU6050_Init();		//MPU6050初始化
+	Key_Init();
 	
-	/*显示ID号*/
-	OLED_ShowString(1, 1, "ID:");		//显示静态字符串
-	ID = MPU6050_GetID();				//获取MPU6050的ID号
-	OLED_ShowHexNum(1, 4, ID, 2);		//OLED显示ID号
+	OLED_ShowHexNum(1, 1, Myflash_ReadWord(0x08000000), 8);
+	OLED_ShowHexNum(2, 1, Myflash_ReadHalfWord(0x08000000), 8);
+	OLED_ShowHexNum(3, 1, Myflash_ReadByte(0x08000000), 8);
+
+	MyFLASH_ErasePage(0x0800FC00);
+	MyFLASH_ProgramWord(0x0800FC00, 0x12345678);
+	MyFLASH_ProgramHalfWord(0x0800FC10, 0xABCD);
 	
 	while (1)
 	{
-		MPU6050_GetData(&AX, &AY, &AZ, &GX, &GY, &GZ);		//获取MPU6050的数据
-		OLED_ShowSignedNum(2, 1, AX, 5);					//OLED显示数据
-		OLED_ShowSignedNum(3, 1, AY, 5);
-		OLED_ShowSignedNum(4, 1, AZ, 5);
-		OLED_ShowSignedNum(2, 8, GX, 5);
-		OLED_ShowSignedNum(3, 8, GY, 5);
-		OLED_ShowSignedNum(4, 8, GZ, 5);
-	}
+		KeyNum = Key_GetNum();
+		if(KeyNum == 1)
+		{
+			MyFLASH_EraseALLPages();
+		}
+		if(KeyNum == 2)
+		{
+			MyFLASH_ErasePage(0x08000000);
+		}
+	}		
 }
