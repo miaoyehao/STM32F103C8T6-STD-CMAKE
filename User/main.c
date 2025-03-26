ffa4ab4dd453aade02cbb885a63ea55a4e81c0ae
miaoyehao <2394785879@qq.com>
2025-03-26 17:11:25 +0800

    UPdate

--- User/main.c
+++ User/main.c
@@ -3,6 +3,7 @@
 #include "OLED.h"
 #include "MyFLASH.h"
 #include "Key.h"
+#include "Store.h"
 
 uint8_t KeyNum;
 
@@ -11,25 +12,44 @@ int main(void)
 	/*模块初始化*/
 	OLED_Init();		//OLED初始化
 	Key_Init();
-	
-	OLED_ShowHexNum(1, 1, Myflash_ReadWord(0x08000000), 8);
+	Store_Init();
+
+	OLED_ShowString(1, 1, "Flag:");
+	OLED_ShowString(1, 1, "Data:");
+
+	/*OLED_ShowHexNum(1, 1, Myflash_ReadWord(0x08000000), 8);
 	OLED_ShowHexNum(2, 1, Myflash_ReadHalfWord(0x08000000), 8);
 	OLED_ShowHexNum(3, 1, Myflash_ReadByte(0x08000000), 8);
 
 	MyFLASH_ErasePage(0x0800FC00);
 	MyFLASH_ProgramWord(0x0800FC00, 0x12345678);
-	MyFLASH_ProgramHalfWord(0x0800FC10, 0xABCD);
+	MyFLASH_ProgramHalfWord(0x0800FC10, 0xABCD);*/
 	
+
+
 	while (1)
 	{
 		KeyNum = Key_GetNum();
 		if(KeyNum == 1)
 		{
-			MyFLASH_EraseALLPages();
+			Store_Data[0] ++;
+			Store_Data[1] += 1;
+			Store_Data[2] += 2;
+			Store_Data[3] += 3;
+			Store_Data[4] += 4;
+			Store_Save();
+			//MyFLASH_EraseALLPages();
 		}
 		if(KeyNum == 2)
 		{
-			MyFLASH_ErasePage(0x08000000);
+			Store_Clear();
+			//MyFLASH_ErasePage(0x08000000);
 		}
+		OLED_ShowHexNum(1,6,Store_Data[0],4);
+		OLED_ShowHexNum(3,1,Store_Data[1],4);
+		OLED_ShowHexNum(3,6,Store_Data[2],4);
+		OLED_ShowHexNum(4,1,Store_Data[3],4);
+		OLED_ShowHexNum(4,6,Store_Data[4],4);
+		
 	}		
 }
